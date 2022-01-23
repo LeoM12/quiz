c3d8a2bb0ed739480847ddf542a8f406d6ac2106
LeoM12 <leospc1@gmail.com>
2022-01-23 13:26:05 +0100

    New paragraph if prerequisites exist

--- javascript/results_script.js
+++ javascript/results_script.js
@@ -1,9 +1,9 @@
 var data = {
-
   A1: {
     name: "Sportbootführerschein (SBF) Binnen Motor",
     description:
       "Amtliche Fahrerlaubnis zum Führen von Sportbooten (unter Motor) unter 20 Meter Länge auf den Binnenschifffahrtsstraßen (Rhein 15 m), vorgeschrieben für Fahrzeuge unter Motor mit mehr als 15 PS.",
+    requirement: "None",
     img: "img/SBF_Binnen_Motor.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/motorbootbinnen/",
   },
@@ -11,6 +11,7 @@ var data = {
     name: "Sportbootführerschein Binnen Segel + Motor",
     description:
       "Amtliche Fahrerlaubnis zum Führen von Sportbooten (unter Motor oder Segel) unter 20 Meter Länge auf den Binnenschifffahrtsstraßen (Rhein unter 15 m), vorgeschrieben für Fahrzeuge unter Motor mit mehr als 15 PS (Rhein über 5 PS). In Berliner Gewässern ist der Sportbootführerschein Binnen (Segeln) vorgeschrieben.",
+    requirement: "None",
     img: "img/SKS_Segel.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/theorie-segeln-binnen/",
   },
@@ -18,6 +19,7 @@ var data = {
     name: "Sportbootführerschein Binnen Segel + Motor",
     description:
       "Amtliche Fahrerlaubnis zum Führen von Sportbooten (unter Motor oder Segel) unter 20 Meter Länge auf den Binnenschifffahrtsstraßen (Rhein unter 15 m), vorgeschrieben für Fahrzeuge unter Motor mit mehr als 15 PS (Rhein über 5 PS). In Berliner Gewässern ist der Sportbootführerschein Binnen (Segeln) vorgeschrieben.",
+    requirement: "None",
     img: "img/SBF_Binnen_Segel.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/theorie-segeln-binnen/",
   },
@@ -25,88 +27,99 @@ var data = {
     name: "Sportbootführerschein (SBF) See",
     description:
       "Amtliche Fahrerlaubnis zum Führen von motorisierten Sportbooten auf den Seeschifffahrtsstraßen. Vorgeschrieben für Fahrzeuge unter Motor mit mehr als 15 PS.",
+    requirement: "None",
     img: "img/SBF_See_Motor.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/motorbootkueste/",
   },
-    //B2 und AB2 nicht enthalten, weil durch mehrere Führerscheine ersetzt wird
+  //B2 und AB2 nicht enthalten, weil durch mehrere Führerscheine ersetzt wird
   A3: {
     name: "Sportseeschifferschein (SSS)",
     description:
-      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern. Voraussetzung: SBF See und 1000 sm",
+      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern.",
+    requirement: "Voraussetzung: SBF See und 1000 Seemeilen",
     img: "img/SSS_Motor.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/sportseeschifferschein/",
   },
   B3: {
     name: "Sportseeschifferschein (SSS)",
     description:
-      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern. Voraussetzung: SBF See und 1000 sm",
-    img: "img/SSS_Segel.jpg",
+      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern.",
+      requirement: "Voraussetzung: SBF See und 1000 Seemeilen",
+      img: "img/SSS_Segel.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/sportseeschifferschein/",
   },
   AB3: {
     name: "Sportseeschifferschein (SSS)",
     description:
-      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern. Voraussetzung: SBF See und 1000 sm",
-    img: "img/SSS_Segel.jpg",
+      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in küstennahen Seegewässern (alle Meere bis 30 sm und Ost- und Nordsee, Mittelmeer und Schwarzes Meer). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in den küstennahen Seegewässern.",
+      requirement: "Voraussetzung: SBF See und 1000 Seemeilen",
+      img: "img/SSS_Segel.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/sportseeschifferschein/",
   },
   A4: {
     name: "Bodenseeschifferpatent (A)",
     description:
       "Vorgeschrieben für Fahrzeuge auf dem Bodensee mit Maschinenantrieb über 4,4 kW.",
-    img: "img/Bodensee_A.jpg",
+      requirement: "None",
+      img: "img/Bodensee_A.jpg",
     link: "https://www.abc-wassersport.de/onlinekurse/onlinekurs/",
   },
   B4: {
     name: "Bodenseeschifferpatent (D)",
     description:
       "Vorgeschrieben für Segelfahrzeuge auf dem Bodensee mit mehr als 12 qm Segelfläche.",
-    img: "img/SBF_Binnen_Segel.jpg",
+      requirement: "None",
+      img: "img/SBF_Binnen_Segel.jpg",
     link: "https://www.abc-wassersport.de/onlinekurse/onlinekurs/",
   },
   AB4: {
     name: "Bodenseeschifferpatent (A+D)",
     description:
       "Vorgeschrieben für Fahrzeuge auf dem Bodensee mit Maschinenantrieb über 4,4 kW sowie Segelfahrzeug mit mehr als 12m² Segelfläche.",
-    img: "img/SKS_Segel.jpg",
+      requirement: "None",
+      img: "img/SKS_Segel.jpg",
     link: "https://www.abc-wassersport.de/onlinekurse/onlinekurs/",
   },
-  SKS:{
+  SKS: {
     name: "Sportküstenschifferschein (SKS)",
     description:
-      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in Küstengewässern (alle Meere bis 12 sm Abstand von der Küste). Voraussetzung: SBF See",
-    img: "img/Bodensee_D.jpg",
+      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in Küstengewässern (alle Meere bis 12 sm Abstand von der Küste).",
+      requirement: "Voraussetzung: SBF See",
+      img: "img/Bodensee_D.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/segelscheinkueste/",
   },
-  SHS:{
+  SHS: {
     name: "Sporthochseeschifferschein (SHS)",
     description:
-      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in der weltweiten Fahrt (alle Meere). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in der weltweiten Fahrt. Voraussetzung: SSS",
-    img: "img/SKS_Segel.jpg",
+      "Amtlicher, empfohlener Führerschein zum Führen von Yachten mit Motor und unter Segel in der weltweiten Fahrt (alle Meere). Vorgeschrieben zum Führen von gewerbsmäßig genutzten Sportbooten in der weltweiten Fahrt.",
+      requirement: "Voraussetzung: SSS",
+      img: "img/SKS_Segel.jpg",
     link: "https://www.abc-wassersport.de/segelscheine/segelscheinkueste/",
   },
-  SRC:{
+  SRC: {
     name: "SRC Funkzeugnis",
     description:
       "Amtliches Funkzeugnis zum Betrieb einer UKW-Funkstelle im Seefunkdienst im Weltweiten Seenot- und Sicherheitsfunksystem GMDSS (Reichweite bis ca. 35 sm) auf Sportbooten.",
-    img: "img/SRC2.jpg",
+      requirement: "None",
+      img: "img/SRC2.jpg",
     link: "https://www.abc-wassersport.de/funkzeugnis/src/",
   },
-  LRC:{
+  LRC: {
     name: "LRC Funkzeugnis",
     description:
       "Amtliches Funkzeugnis zur uneingeschränkten Teilnahme am Seefunkdienst im GMDSS für UKW, Grenzwelle, Kurzwelle und Seefunk über Satelliten (Inmarsat) auf Sportbooten.",
-    img: "img/LRC2.jpg",
+      requirement: "None",
+      img: "img/LRC2.jpg",
     link: "https://www.abc-wassersport.de/funkzeugnis/lrc/",
   },
-  UBI:{
+  UBI: {
     name: "UBI Funkzeugnis",
     description:
       "Amtlicher Funkschein zum Betrieb einer UKW Schiffsfunkstelle auf Binnenschifffahrtsstraßen.",
-    img: "img/UBI2.jpg",
+      requirement: "None",
+      img: "img/UBI2.jpg",
     link: "https://www.abc-wassersport.de/funkzeugnis/ubi/",
   },
-  
 };
 
 var retrievedData = sessionStorage.getItem("match");
@@ -155,9 +168,12 @@ function createLicenseCard(match){
   newParent.appendChild(textCont);
 
   //Add Text blocks and Button
-  addElement("h3", data[match].name, "name", textCont);
-  addElement("p", data[match].description, "description", textCont, match + "Description");
-  addElement("button", "Mehr erfahren", "read-more-btn", textCont, match + "Button");
+  addElement("h3", data[match].name, "name", textCont, match);
+  addElement("p", data[match].description, "description", textCont, match, match + "Description");
+  if (data[match].requirement != "None") {
+    addElement("p", data[match].requirement, "requirements", textCont, match, match + "Requirements");
+  }
+  addElement("button", "Mehr erfahren", "read-more-btn", textCont, match, match + "Button");
 
   licContainer.appendChild(newParent);
   const btn = document.getElementById(match + "Button");
@@ -177,15 +193,16 @@ function addImg(src, parent) {
   parent.appendChild(imgCont);
 }
 
-function addElement(element, content, className, parent, id) {
+function addElement(element, content, className, parent, curMatch, id) {
   var newElement = document.createElement(element);
   var newContent = document.createTextNode(content);
   newElement.classList.add(className);
-  if (content.length > 290 && content.length < 320) {
-    newElement.classList.add("long-text");
-  } else if(content.length >= 320){
-    newElement.classList.add("very-long-text");
+  if(content.length >= 320 || ((content.length > 280) && (data[curMatch].requirement != "None"))){
+    parent.classList.add("very-long-text");
+  } else if(content.length > 290 && content.length < 320){
+    parent.classList.add("long-text");
   }
+
   if (id != null) {
     newElement.id = id;
   }
